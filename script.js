24c8f5765b6e9c7186e1543111cd35bbda1052e6
kimjohnill <112470453+kimjohnill@users.noreply.github.com>
2023-10-31 20:54:26 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 window.fadeIn = function(obj) {
     $(obj).fadeIn(1000);
 }
+
+
+
+/* home slide show */
+
+var $slides = $('.box');
+var slide_pos = 0;
+var slide_len = $('.box').length;
+
+$slides.filter(':gt(0)').hide();
+
+$slides.eq(1).find('img').load(function(){ //wait for second image to load THEN start slideshow
+    slide_int = setInterval(function() {
+        $('#next').click();
+    }, 8000);
+});
+
+
+$('#next').click(function() {
+    $slides.eq(slide_pos % slide_len).fadeOut(500);
+    $slides.eq(++slide_pos % slide_len).fadeIn(500);
+    
+    //clearInterval(slide_int);
+});
+
+$('#prev').click(function() {
+    $slides.eq(slide_pos % slide_len).fadeOut(500);
+    $slides.eq(--slide_pos % slide_len).fadeIn(500);
+    
+    //clearInterval(slide_int);
+});
