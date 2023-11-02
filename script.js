dbbdf6e794dae837fa0ae0b31b6d8133b6beb38f
kimjohnill <112470453+kimjohnill@users.noreply.github.com>
2023-11-02 18:36:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ window.fadeIn = function(obj) {
     $(obj).fadeIn(1000);
 }
 
-$(document).ready(function () {
-    $('#box').css('opacity',0).animate({opacity:1}, 1000);
-}
+var $images = $('div img');
+var loaded_images_count = 0;
+
+$images.load(function(){
+    loaded_images_count++;
+
+    if (loaded_images_count == $images.length) {
+        $("div").show();
+    }
+});
