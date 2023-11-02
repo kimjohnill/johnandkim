7269e22b6a4b917e9fd20dbbbe26bd76f3fe1bbd
kimjohnill <112470453+kimjohnill@users.noreply.github.com>
2023-11-02 19:42:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
-
-
-$('#box').imagesLoaded({
-    progress: function (isBroken, $allImages, $loadedImages, $brokenImages) {
-        console.log('ok: ' + $loadedImages.length + ', bad: ' + $brokenImages.length);
-    },
-    done: function ($images) {
-        $('#box').animate({'opacity':1}, 300);
-    }
-});
+<script>
+    $(function(){$("#box").show("slow");
+                });
+</script>
