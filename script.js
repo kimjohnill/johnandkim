be4a86fc4b73f27101c3e6acf8fe9f364b3ba212
kimjohnill <112470453+kimjohnill@users.noreply.github.com>
2023-11-02 18:20:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ window.fadeIn = function(obj) {
     $(obj).fadeIn(1000);
 }
 
-$(window).load(function () {
-  $('#box').css('visibility','visible');
-}
+$(document).ready(function () {
+    $('#box').css('opacity',0).animate({opacity:1}, 1000);
+});
